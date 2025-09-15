f4867f0b2b0daf88eb1c576ef0472af6bce6bb76
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-15 14:26:48 +0700

    Add ChatMessageDTO class for chat message data transfer

---
+++ src/main/java/com/haraif/spring_websocket/dto/ChatMessageDTO.java
@@ -0,0 +1,22 @@
+package com.haraif.spring_websocket.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ChatMessageDTO {
+  private String id;
+
+  @NotBlank
+  private String sender;
+
+}
