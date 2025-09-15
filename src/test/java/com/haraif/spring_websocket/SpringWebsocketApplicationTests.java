b64d3cb4cb6ffca70219a4b65165630ec4ac41b0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-15 11:09:05 +0700

    Initialize Spring WebSocket project with Maven wrapper and basic configuration

---
+++ src/test/java/com/haraif/spring_websocket/SpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haraif.spring_websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
