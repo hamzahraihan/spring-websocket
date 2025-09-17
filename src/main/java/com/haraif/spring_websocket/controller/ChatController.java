598547a484e144c412a413b214b336afe405c489
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-17 16:22:19 +0700

    Refactor ChatController to handle messages via ChatService and remove unused SendTo annotation

--- src/main/java/com/haraif/spring_websocket/controller/ChatController.java
+++ src/main/java/com/haraif/spring_websocket/controller/ChatController.java
@@ -1,18 +1,23 @@
 package com.haraif.spring_websocket.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.haraif.spring_websocket.dto.ChatMessageDTO;
+import com.haraif.spring_websocket.service.ChatService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class ChatController {
 
+  @Autowired
+  private ChatService chatService;
+
   @MessageMapping("/chat.send")
-  @SendTo("/topic/public")
-  public ChatMessageDTO sendMessage(ChatMessageDTO message) {
-    return message;
+  public void sendMessage(@Valid ChatMessageDTO message) {
+    chatService.handleMessage(message);
   }
 
 }
