8841c348a6f7c873862604750a1cf68fbf25fe98
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-17 16:22:26 +0700

    Refactor ChatMessageDTO to include messageType for better message representation

--- src/main/java/com/haraif/spring_websocket/dto/ChatMessageDTO.java
+++ src/main/java/com/haraif/spring_websocket/dto/ChatMessageDTO.java
@@ -1,22 +1,67 @@
 package com.haraif.spring_websocket.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.haraif.spring_websocket.util.MessageType;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.Size;
 
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChatMessageDTO {
-  private String id;
 
-  @NotBlank
+  @NotBlank(message = "sender required")
+  @Size(max = 50)
   private String sender;
+
+  @Size(max = 100)
+  private String content;
+
+  private long timestamp;
+  private MessageType messageType;
+
+  public ChatMessageDTO() {
+
+  }
+
+  public ChatMessageDTO(
+      String sender,
+      String content,
+      long timestamp,
+      MessageType messageType) {
+    this.sender = sender;
+    this.content = content;
+    this.timestamp = timestamp;
+    this.messageType = messageType;
+  }
+
+  public String getSender() {
+    return this.sender;
+  }
+
+  public void setSender(String sender) {
+    this.sender = sender;
+  }
+
+  public String getContent() {
+    return this.content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public long getTimestamp() {
+    return this.timestamp;
+  }
+
+  public void setTimestamp(long timestamp) {
+    this.timestamp = timestamp;
+  }
+
+  public MessageType getMessageType() {
+    return this.messageType;
+  }
+
+  public void setMessageType(MessageType messageType) {
+    this.messageType = messageType;
+  }
 
 }
