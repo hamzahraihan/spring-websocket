6160ad124a5f30b2dfbdbb5542089f8cfaef52db
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-17 19:35:32 +0700

    Refactor ChatController to improve message handling and logging

--- src/main/java/com/haraif/spring_websocket/controller/ChatController.java
+++ src/main/java/com/haraif/spring_websocket/controller/ChatController.java
@@ -2,22 +2,34 @@ package com.haraif.spring_websocket.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 import com.haraif.spring_websocket.dto.ChatMessageDTO;
+import com.haraif.spring_websocket.model.ChatMessage;
 import com.haraif.spring_websocket.service.ChatService;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class ChatController {
 
   @Autowired
   private ChatService chatService;
 
+  @Autowired
+  private SimpMessagingTemplate messagingTemplate;
+
   @MessageMapping("/chat.send")
-  public void sendMessage(@Valid ChatMessageDTO message) {
-    chatService.handleMessage(message);
+  public void sendMessage(@Valid ChatMessageDTO messageDto) {
+    ChatMessage message = chatService.processMessage(messageDto);
+
+    log.info("new message from {}: {}", message.getSender(), message.getContent());
+
+    messagingTemplate.convertAndSend(message);
   }
 
 }
