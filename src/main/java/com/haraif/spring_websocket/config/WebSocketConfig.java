c6fec31b1d6f687b42862c5d99f93401c143308b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-09-18 15:10:37 +0700

    Refactor WebSocketConfig to improve endpoint configuration and maintain consistent application destination prefixes

--- src/main/java/com/haraif/spring_websocket/config/WebSocketConfig.java
+++ src/main/java/com/haraif/spring_websocket/config/WebSocketConfig.java
@@ -7,16 +7,19 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
-    // message whose destination starts with "/app" routed to @MessageMapping
-    config.setApplicationDestinationPrefixes("/app");
-
     // simple in-memory broker; clients subscribe to destination like "/topic/*"
     config.enableSimpleBroker("/topic", "/queue");
+
+    // message whose destination starts with "/app" routed to @MessageMapping
+    config.setApplicationDestinationPrefixes("/app");
   }
 
   @Override
@@ -24,7 +27,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // clients connect to "/ws" sockJS fallback enabled
     registry
         .addEndpoint("/ws")
-        .setAllowedOriginPatterns("http://localhost:5173", "http://localhost:3000")
+        .setAllowedOrigins("http://localhost:5173")
+        .setAllowedOriginPatterns("*")
         .withSockJS();
   }
+
 }
